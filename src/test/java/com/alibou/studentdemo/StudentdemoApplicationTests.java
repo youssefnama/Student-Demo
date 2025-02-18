12b43d0e4d0a02109f274e1b9fb8d17dac8a967f
Youssef <youssef.nama.dev@gmail.com>
2025-02-18 19:11:29 +0100

    initializing repository

---
+++ src/test/java/com/alibou/studentdemo/StudentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.studentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
